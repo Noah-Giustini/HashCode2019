773484601e40031eaff7975e8fbbc41d4768b2de
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2019-02-28 11:29:05 -0700

    Create Photo.java
    Photo class with getters and constructor

---
+++ Photo.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList<E>;
+
+public class Photo{
+
+        private String orientation;
+        private int numTags;
+        private ArrayList<String> tags;
+        private int identifier;
+
+        public Photo (String[] line, int lineNum){
+                this.orientation = line[0];
+                this.numTags = line[1];
+                for (i=2, i++, line.length - 1){
+                        tags.add(line[i]);
+                }
+        }
+
+        public String getOrientation(){
+                return this.orientation;
+        }
+        public int getNumTags(){
+                return this.numTags;
+        }
+        public ArrayList<string> getTags(){
+                return this.tags;
+        }
+        public int getIdentifier(){
+                return this.identifier;
+        }
+}
