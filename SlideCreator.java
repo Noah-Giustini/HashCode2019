be213eddb64b3883f755b5cfa3bfe47601e20199
kaitlin.decf <kaitlin.decf@gmail.com>
2019-02-28 13:17:11 -0700

    Update SlideCreator.java

--- SlideCreator.java
+++ SlideCreator.java
@@ -4,119 +4,111 @@ import java.io.*;
 import java.util.*;
 
 public class SlideCreator {
-    
-    
-    public static void main (String[] args ) throws FileNotFoundException{
+
+    public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Photo> verticalPhotos = new ArrayList<Photo>();
         ArrayList<Photo> horizontalPhotos = new ArrayList<Photo>();
-        
+
         String fileName = "a_example.txt";
-        File theFile  = new File(fileName);
+        File theFile = new File(fileName);
         Scanner reader = new Scanner(theFile);
         int i = 0;
-        int total =  reader.nextInt();
-        
+        int total = reader.nextInt();
+
         //Sort the photos into Horizontal or Vertial
-        while (reader.hasNext()){
+        while (reader.hasNext()) {
             String theLine = reader.nextLine();
             //System.out.println(theLine);
             String[] data = theLine.split(" ");
-    
-            if (data[0].equals("H")){
+
+            if (data[0].equals("H")) {
                 //printArray(data);
                 horizontalPhotos.add(new Photo(data, i));
             } else {
-                 //printArray(data);
+                //printArray(data);
                 verticalPhotos.add(new Photo(data, i));
             }
             i++;
         }
-        
-        
-        
+
         //Generate the Slides 
         ArrayList<Slide> horizontalSlides = new ArrayList<Slide>();
-        
-        
-        
+
         ArrayList<Slide> verticalSlides = VerticalCombo(verticalPhotos);
-        
+
         //Combine all Slides and place in Heap order 
-        
-        
         //HeapSort all slides so its maxtags to least tags 
-        
-        
-        
         //Go through the ArrayList and  compare the first one to the next to find the best pair
-        
         ArrayList<Slide> orderedSlidesByNumOfTags = new ArrayList<Slide>();
-        int currBest = 0; 
+        int currBest = 0;
         int idxOfBest = 0;
         Slide firstSlide = orderedSlidesByNumOfTags.get(0);
         ArrayList<Slide> finalShow = new Arraylist<Slide>;
-        
-        while (!orderedSlidesByNumOfTags.size() == 1){
+
+        while (orderedSlidesByNumOfTags.size() != 1) {
             currBest = 0;
             idxOfBest = 0;
             ArrayList<String> firstTags = firstSlide.getTags();
             int firstTagNum = firstSlide.getNumTags();
-            
-            
-            for (int j = 1; j< orderedSlidesByNumOfTags.size(); j++){
+
+            for (int j = 1; j < orderedSlidesByNumOfTags.size(); j++) {
                 Slide second = orderedSlidesByNumOfTags.get(j);
-                
+
                 int secondTagNum = second.getNumTags();
                 ArrayList<String> secondTags = second.getTags();
                 int both = 0;
-                int onlyFirst = 0; 
-                int onlySecond = 0; 
-                
-                if (firstTagNum < secondTagNum){
-                    for (int e = 0; e < firstTagNum; e++){
-                        if (secondTags.contains(firstTags.get(e))){
+                int onlyFirst = 0;
+                int onlySecond = 0;
+
+                if (firstTagNum < secondTagNum) {
+                    for (int e = 0; e < firstTagNum; e++) {
+                        if (secondTags.contains(firstTags.get(e))) {
                             both++;
-                        }else{
+                        } else {
                             onlyFirst++;
                         }
-                        
+
                     }
                     onlySecond = secondTagNum - both;
-                    
-                    int calc = Math.min(both, Math.min(onlyFirst, onlySecond));
-                    
-                    if (calc > currBest){
-                        currBest = calc;
-                        idxOfBest = j;
-                    } 
-                    
-                    if (calc  < secondTagNum/2){
-                        break;
+                } else {
+                    for (int e = 0; e < secondTagNum; e++) {
+                        if (firstTags.contains(secondTags.get(e))) {
+                            both++;
+                        } else {
+                            onlySecond++;
+                        }
+
                     }
-                    
-                    
+                    onlyFirst = firstTagNum - both;
                 }
-                
-                
-                
-                
-                
-                
+
+                int calc = Math.min(both, Math.min(onlyFirst, onlySecond));
+
+                if (calc > currBest) {
+                    currBest = calc;
+                    idxOfBest = j;
+                }
+
+                if (calc < secondTagNum / 2) {
+                    break;
+                }
+
             }
+
             finalShow.add(orderedSlidesByNumOfTags.get(0));
-            orderedSlidesByNumOfTags.set(0,orderedSlidesByNumOfTags.get(idxOfBest));
-            orderedSlidesByNumOfTags.remove(idxOfBest);
+            orderedSlidesByNumOfTags.set(0, orderedSlidesByNumOfTags.get(idxOfBest));
+            orderedSlidesByNumOfTags.remove(orderedSlidesByNumOfTags.get(idxOfBest));
+
         }
-        finalShow.add(orderedSlidesByNumOfTags.get(0));    
-    
+        finalShow.add(orderedSlidesByNumOfTags.get(0));
+
     }
-    
-    public static void printArray(String[] a){
-        for (int i = 0; i< a.length; i++){
-            System.out.print(a[i]+ ", ");
+
+    public static void printArray(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ", ");
         }
         System.out.println();
     }
-
 
 }
