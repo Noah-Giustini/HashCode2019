b312f9d39c0c2d26b2d35aff6e8089714df01b88
kaitlin.decf <kaitlin.decf@gmail.com>
2019-02-28 11:31:04 -0700

    Update SlideCreator.java

--- SlideCreator.java
+++ SlideCreator.java
@@ -10,22 +10,25 @@ public class SlideCreator {
         ArrayList<Photo> verticalPhotos = new ArrayList<Photo>();
         ArrayList<Photo> horizontalPhotos = new ArrayList<Photo>();
         
-        String fileName = "";
+        String fileName = "a_example.txt";
         File theFile  = new File(fileName);
         Scanner reader = new Scanner(theFile);
         int i = 0;
         int total =  reader.nextInt();
+        
+        //Sort the photos into Horizontal or Vertial
         while (reader.hasNext()){
             String theLine = reader.nextLine();
-            String[] data = theLine.split(theLine);
+            //System.out.println(theLine);
+            String[] data = theLine.split(" ");
+    
             if (data[0].equals("H")){
+                //printArray(data);
                 horizontalPhotos.add(new Photo(data, i));
             } else {
+                 //printArray(data);
                 verticalPhotos.add(new Photo(data, i));
             }
-            
-            
-            
             i++;
         }
         
@@ -34,6 +37,13 @@ public class SlideCreator {
         
     
     
+    }
+    
+    public static void printArray(String[] a){
+        for (int i = 0; i< a.length; i++){
+            System.out.print(a[i]+ ", ");
+        }
+        System.out.println();
     }
 
 
